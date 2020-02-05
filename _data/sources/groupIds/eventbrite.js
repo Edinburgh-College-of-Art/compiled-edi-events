1d947057220980781bf8466cb854388a07aaac68
Sean O'Mahoney <sean@inevitableteam.com>
2020-02-05 18:12:39 +0000

    Corrected Eventbrite Group Id
    Apologies for making it unclear in the documentation, the id property will be the same number at the end of the url. The Id is used to get events from the Eventbrite API, then the URL is just for the links when displayed.

--- _data/sources/groupIds/eventbrite.js
+++ _data/sources/groupIds/eventbrite.js
@@ -1,5 +1,5 @@
 module.exports = [{
-    id: 5881267695,
+    id: 18717122091,
     name: "Creative Informatics",
     img: "https://pbs.twimg.com/profile_images/1073220455035736064/xjepww4M_400x400.jpg",
     url: "https://www.eventbrite.co.uk/o/creative-informatics-18717122091"
