b1986f6dd6c8df37d94548a8c015be5b5812ffa5
Evan Morgan <e.morgan@ed.ac.uk>
2020-02-26 19:18:27 +0000

    add new sources

--- _data/sources/groupIds/eventbrite.js
+++ _data/sources/groupIds/eventbrite.js
@@ -1,6 +1,12 @@
 module.exports = [{
-    id: 5881267695,
+    id: 18717122091,
     name: "Creative Informatics",
     img: "https://pbs.twimg.com/profile_images/1073220455035736064/xjepww4M_400x400.jpg",
     url: "https://www.eventbrite.co.uk/o/creative-informatics-18717122091"
-}];
+  }, {
+    id: 12335923204,
+    name: "FutureX",
+    img: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F89742275%2F55971866509%2F2%2Foriginal.png",
+    url: "https://www.eventbrite.co.uk/o/futurex-12335923204"
+  }
+];
